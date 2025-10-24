9d35da03cbe99df6aa75f2068f3670ab35e7f7f8
Bindu priya <pehlaj0405@outlook.com>
2025-10-24 17:36:00 +0530

    new changes

--- variables.tf
+++ variables.tf
@@ -7,7 +7,3 @@ variable "base_name" {
   type    = string
   default = "ghdemo"   # used to build names; final name will include random suffix
 }
-variable "subscription_id" {
-  type        = string
-  description = "Azure subscription ID" #for testing
-}
